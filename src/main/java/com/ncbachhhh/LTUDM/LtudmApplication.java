82ae72a97754ab2b48a3c81af8de2d10cffdd676
ncbachhhh <nguyenchienbach123@gmail.com>
2026-01-20 11:15:04 +0700

    first commit

---
+++ src/main/java/com/ncbachhhh/LTUDM/LtudmApplication.java
@@ -0,0 +1,13 @@
+package com.ncbachhhh.LTUDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtudmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtudmApplication.class, args);
+	}
+
+}
